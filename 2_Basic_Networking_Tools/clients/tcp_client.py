1378e2f341d6fa1badb7a9573e37c811057e3304
brist0l <gautamthegreat21@gmail.com>
2022-11-29 15:13:36 +0000

    Added tcp server

--- 2_Basic_Networking_Tools/clients/tcp_client.py
+++ 2_Basic_Networking_Tools/clients/tcp_client.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "0.0.0.0"
+target_port = 6969 
 
 #create a socket object 
 # 1-> IPv4
@@ -12,7 +12,7 @@ client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect((target_host,target_port))
 
 #send some data
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"Hemlo :)")
 
 #receive some data
 response = client.recv(4096)
